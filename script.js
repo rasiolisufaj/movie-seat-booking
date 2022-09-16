fa4f50ab43fea1a37fc31e113f7eb7668ef2f6b4
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2022-09-16 18:52:00 +0200

    local storage

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let countElement = document.getElementById("count");
 let totalPriceElement = document.getElementById("total-price");
 let ticketPrice = +selectElement.value;
 
+populateUI();
+
 /* Seat state initialization
 
 const seats = [];
@@ -25,14 +27,43 @@ for(let i = 0; i< SEAT_NUMER; i++){
 
 */
 
+// Set Movie Data
+setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem("movieIndex", movieIndex);
+  localStorage.setItem("moviePrice", moviePrice);
+};
+
 // Update count and total
-updateSeatAndPrice = () => {
+function updateSeatAndPrice() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   let count = selectedSeats.length;
   let price = count * ticketPrice;
   countElement.innerText = count;
   totalPriceElement.innerText = price;
-};
+
+  // Save Data To Local Storage
+  const seatsIndex = [...selectedSeats].map((seat) =>
+    [...availableSeats].indexOf(seat)
+  );
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+}
+
+// Populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    availableSeats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("movieIndex");
+  if (selectedMovieIndex !== null) {
+    selectElement.selectedIndex = selectedMovieIndex;
+  }
+}
 
 // Seat Selecting
 containerElement.addEventListener("click", (e) => {
@@ -47,6 +78,10 @@ containerElement.addEventListener("click", (e) => {
 
 // Price Updating
 selectElement.addEventListener("change", (e) => {
+  setMovieData(e.target.selectedIndex, e.target.value);
   ticketPrice = +e.target.value;
   updateSeatAndPrice();
 });
+
+// Count and TotalPrice Set
+updateSeatAndPrice();
